170376b747e5dd3a0304db1da53254f81b47b8fe
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2022-01-30 23:10:25 +0900

    numberの接頭辞として0を追加

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -64,7 +64,7 @@ pub fn tokenize(str_vec: Vec<char>) -> Result<Vec<Token>, TokenizeError> {
             'n' => {
                 distinguish_null(&str_vec, &mut count, &mut vec)?;
             }
-            '-' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+            '-' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
                 distinguish_number(&str_vec, &mut count, &mut vec);
             }
             '\n' | '\r' | ' ' | '\t' => {
